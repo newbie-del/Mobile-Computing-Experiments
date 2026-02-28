c0c939a3a1c05f441b0003fcc0baf04cf883105b
Itz_jignesh <bsmarty863@gmail.com>
2026-02-28 10:48:35 +0530

    Update CDMA.py

--- EXP8CDMA/CDMA.py
+++ EXP8CDMA/CDMA.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-# Chips/Codes define karna
 c1 = [1, 1, 1, 1]
 c2 = [1, -1, 1, -1]
 c3 = [1, 1, -1, -1]
@@ -13,20 +12,16 @@ d2 = int(input("Enter D2 :"))
 d3 = int(input("Enter D3 :"))
 d4 = int(input("Enter D4 :"))
 
-# Data ko chips se multiply karna
 r1 = np.multiply(c1, d1)
 r2 = np.multiply(c2, d2)
 r3 = np.multiply(c3, d3)
 r4 = np.multiply(c4, d4)
 
-# Saare signals ko jod kar resultant channel banana
 resultant_channel = r1 + r2 + r3 + r4
 print("Resultant Channel", resultant_channel)
 
-# Station select karna
 Channel = int(input("Enter the station to listen for (C1=1, C2=2, C3=3, C4=4): "))
-
-# Indentation (Space) yahan sahi ki gayi hai:
+;
 if Channel == 1:
     rc = c1
 elif Channel == 2:
